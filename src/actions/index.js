95ee6478434cbca3ea6d68f16d4003566c28f5aa
rawwr <akshayace0302@gmail.com>
2022-05-23 18:17:19 +0530

    using thunk middleware

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+import jsonPlaceHolder from "../apis/jsonPlaceHolder";
+
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceHolder.get("/posts");
+  dispatch({ type: "FETCH_POSTS", payload: response });
+};
+
+// export const fetchPosts = () => {
+//     return async function (dispatch) {
+//       const response = await jsonPlaceHolder.get("/posts");
+//       dispatch({ type: "FETCH_POSTS", payload: response });
+//     };
+//   };
