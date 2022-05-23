ed87d3c00b445937b3db841a0806c716edb34378
rawwr <akshayace0302@gmail.com>
2022-05-24 00:52:10 +0530

    list building

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,9 +6,26 @@ class PostList extends Component {
     //store.dispatch(fetchPosts())-->behind the scene
     this.props.fetchPosts();
   }
+
+  renderList() {
+    return this.props.posts.map((post, index) => {
+      return (
+        <div className="item" key={index}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    console.log(this.props.posts);
-    return <div>PostList</div>;
+    // console.log(this.props.posts);
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
