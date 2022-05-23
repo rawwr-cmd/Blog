52f558edb0de9f3bd43e492dfe4be4da57593b4f
rawwr <akshayace0302@gmail.com>
2022-05-23 18:16:10 +0530

    wiring up middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import App from "./components/App";
 import reducers from "./reducers";
 
+const store = createStore(reducers, applyMiddleware(thunk));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>
 );
